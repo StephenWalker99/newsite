381be747a2d35666c372ba610394f73f976a79cb
Stephen Walker <walkerdesigns@virginmedia.com>
2022-05-02 10:15:54 +0100

    Update config.php

--- model/config.php
+++ model/config.php
@@ -2,7 +2,7 @@
 
 define (DB_USER, "root");
 define (DB_PASSWORD, "");
-define (DB_DATABASE, "members");
+define (DB_DATABASE, "whatever");
 define (DB_HOST, "localhost");
 $mysqli = new mysqli(DB_HOST, DB_USER, DB_PASSWORD, DB_DATABASE);
 
